fb4cc39f6ded1111de0f0a86684866fce9764b0f
Chelsea Hatton <chelseahatton1992@gmail.com>
2023-08-30 11:29:34 -0400

    conditionals intro complete

---
+++ main.js
@@ -0,0 +1,57 @@
+/*
+if(1 + 1 > 5){
+    console.log("True fact!");
+} else {
+    console.log("LIES!")
+} 
+
+if(4 + 5 <= 20){
+    console.log("True fact!");
+} else {
+    console.log("LIES!")
+} 
+
+if(6 - 2 >= 0){
+    console.log("True fact!");
+} else {
+    console.log("LIES!")
+} 
+
+const likesDogs = "True";
+if(likesDogs === "True"){
+    console.log("You're a dog person!")
+} else {
+    console.log("You're a cat person!")
+} 
+
+let nameAndLang = {
+    name: 'Patience',
+    language: 'English',
+  }
+
+switch (nameAndLang.language) {
+    case 'Spanish':
+    console.log(`Hola, ${nameAndLang.name}!`);
+    break;
+  case 'French':
+    console.log(`Bonjour, ${nameAndLang.name}!`);
+    break;
+    default:
+    console.log(`Hello, ${nameAndLang.name}`);
+} */
+
+
+let grade = 85;
+
+if(grade >= 0 && grade <= 69){
+    console.log("You got an F");
+} else if (grade >= 70 && grade < 76){
+    console.log("You got a D");
+} else if (grade >= 77 && grade < 84){
+        console.log("You got a C");
+} else if (grade >= 84 && grade < 92){
+    console.log("You got a B");
+} else if (grade >= 93 && grade <= 100){
+    console.log("You got a A");
+}
+
